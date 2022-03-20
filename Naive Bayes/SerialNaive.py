d821a211727129f61961d02eb8e57ab4a3e9650b
SadeceAhmetFaruk1 <cybersteinmicro@gmail.com>
2022-03-21 00:48:11 +0300

    C

--- Naive Bayes/SerialNaive.py
+++ Naive Bayes/SerialNaive.py
@@ -2,11 +2,11 @@
 import pandas as pd
 
 # csv dosyamızı okuduk.
-data = pd.read_csv('ParalellNaiveBayes\Databases\General\Iris.csv')
+data = pd.read_csv('C:\Repos\ParalellNaiveBayes\Databases\Finance\LOGO.IS.csv')
 
 # Bağımlı Değişkeni (species) bir değişkene atadık
 # 4 Finans için  -1 ise Irıs veritabanı için
-species = data.iloc[:,-1:].values #Commit 4 for change something  x"
+species = data.iloc[:,3:].values #Commit 4 for change something  databases"
 
 # Veri kümemizi test ve train şekinde bölüyoruz
 from sklearn.model_selection import train_test_split
@@ -39,5 +39,5 @@ print(cm)
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(y_test, result)
 
-# Sonuç : 0.96
+# Sonuç : 0.96 Iris için 
 print(accuracy)
